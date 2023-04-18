2719323a0a40b33a8f818623d8f6a8830fa59457
AslanAV <autlevaslan@yandex.ru>
2023-04-18 17:23:47 +0300

    add models for: - Mentors

---
+++ app/Models/Mentor.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Mentor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'slug',
+        'name',
+        'email',
+        'telegram_username',
+        'photo_url',
+        'job_title',
+        'workplace',
+        'about',
+        'description',
+        'competencies',
+        'menteeCount',
+        'link_to_calendar',
+        'privacy_policy_agreement',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
