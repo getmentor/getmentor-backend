259bad31690eb959ee99385a4e54502a4568b707
AslanAV <autlevaslan@yandex.ru>
2023-04-25 12:45:01 +0300

    delete methods for store, update, destroy

--- app/Http/Controllers/Api/MentorController.php
+++ app/Http/Controllers/Api/MentorController.php
@@ -15,42 +15,9 @@ class MentorController extends Controller
         $mentors = Mentor::paginate(10);
         return response()->json($mentors);
     }
-    public function store(StoreMentorRequest $request): JsonResponse
-    {
-        $mentor = new Mentor();
-        $validated = $request->validated();
-        $data = [...$validated, 'mentee_count' => 0];
-        $mentor->fill($data);
-        $mentor->save();
-
-        if (array_key_exists('specializations', $validated)) {
-            $mentor->specializations()->attach($validated['specializations']);
-        }
-
-        return response()->json($mentor, 201);
-    }
     public function show($id): JsonResponse
     {
         $mentor = Mentor::findOrFail($id);
         return response()->json($mentor);
-    }
-    public function update(UpdateMentorRequest $request, $id): JsonResponse
-    {
-        $mentor = Mentor::findOrFail($id);
-        $validated = $request->validated();
-        $mentor->fill($validated);
-        $mentor->save();
-
-        if (array_key_exists('specializations', $validated)) {
-            $mentor->specializations()->sync($validated['specializations']);
-        }
-
-        return response()->json($mentor);
-    }
-    public function destroy($id): JsonResponse
-    {
-        $mentor = Mentor::findOrFail($id);
-        $mentor->deleteOrFail();
-        return response()->json([], 204);
     }
 }
