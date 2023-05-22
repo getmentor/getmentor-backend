be02ccf8631289c3549b20fed094bf8aef924719
AslanAV <autlevaslan@yandex.ru>
2023-05-23 01:55:21 +0300

    add hinting in PhpDocs in MentorController.php

--- app/Http/Controllers/Api/MentorController.php
+++ app/Http/Controllers/Api/MentorController.php
@@ -7,17 +7,26 @@ use App\Http\Requests\StoreMentorRequest;
 use App\Http\Requests\UpdateMentorRequest;
 use App\Models\Mentor;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class MentorController extends Controller
 {
+    /**
+     * @return JsonResponse
+     */
     public function index(): JsonResponse
     {
         $mentors = Mentor::paginate(10);
+
         return response()->json($mentors);
     }
     public function show($id): JsonResponse
     {
         $mentor = Mentor::findOrFail($id);
+        /**
+         * @status 200
+         * @body Mentor
+         */
         return response()->json($mentor);
     }
 }
