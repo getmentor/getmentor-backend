83bdd6577548b39ff41151fbbc6c05735a570dc1
AslanAV <autlevaslan@yandex.ru>
2023-06-01 22:04:28 +0300

    edit return collections

--- app/Http/Controllers/Api/MentorController.php
+++ app/Http/Controllers/Api/MentorController.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreMentorRequest;
 use App\Http\Requests\UpdateMentorRequest;
+use App\Http\Resources\MentorCollection;
+use App\Http\Resources\MentorResource;
 use App\Models\Mentor;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class MentorController extends Controller
 {
     public function index()
     {
         $mentors = Mentor::paginate(10);
-        return response()->json($mentors);
+        return new MentorCollection($mentors);
     }
-    public function show($id): JsonResponse
+    public function show($id): JsonResource
     {
         $mentor = Mentor::findOrFail($id);
-        return response()->json($mentor);
+        return new MentorResource($mentor);
     }
 }
