5b97d7edc6ee56c6646f46be1f8ea5d9db216b4b
AslanAV <autlevaslan@yandex.ru>
2023-06-06 22:33:04 +0300

    add Gate to view documentation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Gate::define('viewApiDocs', function (User $user) {
+    return in_array($user->email, ['admin@app.com']);
+});
 
 Route::get('/', function () {
     return view('welcome');
