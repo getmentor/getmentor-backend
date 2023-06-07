5ef3b3655fe20a51477a2d7411513d961ab210dc
feycot <feycot@gmail.com>
2023-06-07 19:31:02 +0500

    fix gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('viewApiDocs', fn() => true);
+        Gate::define('viewApiDocs', function (?User $user = null) {
+            return true;
+        });
     }
 }
